b0588f1730500f18944b60e0f167d706fe3edc42
Внукова Анастасия <nastenka696945@gmail.com>
2023-01-16 20:19:40 +0300

    HW2-2

---
+++ src/game.js
@@ -0,0 +1,16 @@
+export default Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
+
+import { Character } from './domain.js';
